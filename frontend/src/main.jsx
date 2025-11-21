8db94a37c772e7cdaba322e88b08550dd47486d0
vathdev <vthien562004@gmail.com>
2025-11-21 20:57:37 +0700

    fix router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,13 @@ import ReactDOM from 'react-dom/client'
 import React from 'react'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <StoreContextProvider>
-     <App />
-  </StoreContextProvider>
-     
-    </BrowserRouter>
- 
+      <App />
+    </StoreContextProvider>
+  </HashRouter>
 )
